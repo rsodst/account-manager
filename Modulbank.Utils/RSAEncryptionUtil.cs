1681a764ac3358d11dde8200dffc5f167ab477a6
Ринат Сафиуллин <rsodst@gmail.com>
2020-06-07 21:01:10 +0500

    feat: add secure

---
+++ Modulbank.Utils/RSAEncryptionUtil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Modulbank.Utils
+{
+    public static class RSAEncryptionUtil
+    {
+        public static string Encryption(string strText, string publicKey)
+        {
+            var testData = Encoding.UTF8.GetBytes(strText);
+
+            using (var rsa = new RSACryptoServiceProvider(1024))
+            {
+                try
+                {
+                    rsa.FromXmlString(publicKey.ToString());
+
+                    var encryptedData = rsa.Encrypt(testData, true);
+
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public static string Decryption(string strText, string privateKey)
+        {
+            var testData = Encoding.UTF8.GetBytes(strText);
+
+            using (var rsa = new RSACryptoServiceProvider(1024))
+            {
+                try
+                {                    
+                    var base64Encrypted = strText;
+
+                    rsa.FromXmlString(privateKey);
+
+                    var resultBytes = Convert.FromBase64String(base64Encrypted);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+    }
+}
---
+++ src/Infrastructure/Modulbank.Settings/PostgresConnections.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modulbank.Settings
+{
+    public class PostgresConnections
+    {
+        public List<PostgresConnectionOptions> connectionOptions { get; set; }
+    }
+}
